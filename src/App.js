547267d22229488d78fac69baffe0675f1ede88e
diegogalviz <arqdiegogalvizv@gmail.com>
2022-08-21 01:18:27 -0500

    home rick and morty

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import rick_morty from './img/rick-morty.png'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const reqApi = () => {
+        console.log('funcionando')
+    }
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1 className={'title'}>Rick & Morty</h1>
+                <img src={rick_morty} alt="Rick & Morty" className={'img-home'}/>
+                <button onClick={reqApi} className={'btn-search'}>Buscar Personajes</button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
